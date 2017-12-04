bbf6659a0f25de20221e5b98ff8703dc893e3c99
SQLSprint <andrew@coeo.com>
2017-12-04 12:10:48 +0000

    Header
    Added a header.

--- Security/Service Account Permissions.sql
+++ Security/Service Account Permissions.sql
@@ -1,3 +1,20 @@
+ /*
+Name:
+
+(C) Andy Jones
+
+mailto:andrew@coeo.com
+
+Description: -
+
+Show the privileges for the SQL Server service account. This requires xp_cmdshell to be available.
+
+Change History: -
+1.0 04/12/2017 Created.
+
+License: free to download and use providing this header is kept in place. Author consent is required for
+any sale or re-distibution.
+*/
 EXECUTE sp_configure 'show advanced options', '1';
 RECONFIGURE;
 EXECUTE sp_configure 'xp_cmdshell', '1';
@@ -5,3 +22,4 @@ RECONFIGURE;
 EXECUTE xp_cmdshell 'whoami /priv'
 EXECUTE sp_configure 'xp_cmdshell', '0';
 RECONFIGURE;
+GO
